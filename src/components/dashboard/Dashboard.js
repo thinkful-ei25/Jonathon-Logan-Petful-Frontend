6c6559105589fd3fcf961decf82ec89f54952660
Reldalk <Reldalk@gmail.com>
2018-12-21 12:26:07 -0500

    React-router working

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import './Dashboard.css';
+
+export default class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      petArray: [],
+    };
+  }
+  render() {
+    return (
+      <div className = "body">
+        <div className="navBar">
+          <h2 className="navBarTitle">Welcome to our site!</h2>
+        </div>
+        <div>
+          <select className="selection">
+            <option>Select Animal Type</option>
+            <option>Dogs</option>
+            <option>Cats</option>
+            <option>Dogs and Cats</option>
+          </select>
+        </div>
+        <table className="petTable">
+          <tbody>
+            <tr>
+              {}
+              <td className="petImage">
+                <img className="image" src="https://img.buzzfeed.com/buzzfeed-static/static/2015-04/23/17/enhanced/webdr15/enhanced-6210-1429824367-3.jpg?downsize=700:*&output-format=auto&output-quality=auto"></img>
+              </td>
+              <td className="petDescription">
+                <h2 className="header">Testing</h2>
+                <p>Lorem Ipsum Description Text awodihaowie awdeijaw d wad wad wa dwa d awd w</p>
+                <button className="adoptMe">Adopt Me!</button>
+              </td>
+              <td className="petImage">
+              <img className="image" src="https://img.buzzfeed.com/buzzfeed-static/static/2015-04/23/17/enhanced/webdr15/enhanced-6210-1429824367-3.jpg?downsize=700:*&output-format=auto&output-quality=auto"></img>
+              </td>
+              <td className="petDescription">
+
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
+import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './components/app/App';
 import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
