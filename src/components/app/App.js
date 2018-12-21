d3b0aa0ac77887f8d17c8299e81c76160d6d131d
Reldalk <Reldalk@gmail.com>
2018-12-21 12:09:34 -0500

    front end changes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,16 +1,49 @@
 import React, { Component } from 'react';
+import {array} from './testArray.js/array'
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      petArray: [],
+    };
+  }
   render() {
+    console.log(array)
     return (
-      <body className = "body">
+      <div className = "body">
+        <div className="navBar">
+          <h2 className="navBarTitle">Welcome to our site!</h2>
+        </div>
+        <div>
+          <select className="selection">
+            <option>Dogs</option>
+            <option>Cats</option>
+            <option>Dogs and Cats</option>
+          </select>
+        </div>
         <table className="petTable">
-          <row>
+          <tbody>
+            <tr>
+              <td className="petImage">
+                <img className="image" src="https://img.buzzfeed.com/buzzfeed-static/static/2015-04/23/17/enhanced/webdr15/enhanced-6210-1429824367-3.jpg?downsize=700:*&output-format=auto&output-quality=auto"></img>
+              </td>
+              <td className="petDescription">
+                <h2 className="header">Testing</h2>
+                <p>Lorem Ipsum Description Text awodihaowie awdeijaw d wad wad wa dwa d awd w</p>
+                <button className="adoptMe">Adopt Me!</button>
+              </td>
+              <td className="petImage">
+              <img className="image" src="https://img.buzzfeed.com/buzzfeed-static/static/2015-04/23/17/enhanced/webdr15/enhanced-6210-1429824367-3.jpg?downsize=700:*&output-format=auto&output-quality=auto"></img>
+              </td>
+              <td className="petDescription">
 
-          </row>
+              </td>
+            </tr>
+          </tbody>
         </table>
-      </body>
+      </div>
     );
   }
 }
---
+++ src/components/app/testArray.js/array.js
@@ -0,0 +1,84 @@
+let array = [
+  {
+    imgURL: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-04/23/17/enhanced/webdr15/enhanced-6210-1429824367-3.jpg?downsize=700:*&output-format=auto&output-quality=auto',
+    name: 'Bob Ross',
+    sex: 'male',
+    age: '6',
+    type: 'dog',
+    breed: 'Box dog',
+    description: 'Standing less than a foot tall, these sturdy terrier-like dogs approach life with great confidence. “This isn’t a breed you train,” a professional dog handler tells us, “He’s like a human. You befriend him.” The dense, harsh coat is described as “neat but shaggy” and comes in several colors; the gait is light and confident. They can be willful and domineering, but mostly Affens are loyal, affectionate, and always entertaining. Affen people say they love being owned by their little monkey dogs.',
+    adoption: true
+  },
+  {
+    imgURL: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-04/23/17/enhanced/webdr15/enhanced-6210-1429824367-3.jpg?downsize=700:*&output-format=auto&output-quality=auto',
+    name: 'Bob Ross',
+    sex: 'male',
+    age: '6',
+    type: 'dog',
+    breed: 'Box dog',
+    description: 'Standing less than a foot tall, these sturdy terrier-like dogs approach life with great confidence. “This isn’t a breed you train,” a professional dog handler tells us, “He’s like a human. You befriend him.” The dense, harsh coat is described as “neat but shaggy” and comes in several colors; the gait is light and confident. They can be willful and domineering, but mostly Affens are loyal, affectionate, and always entertaining. Affen people say they love being owned by their little monkey dogs.',
+    adoption: true
+  },
+  {
+    imgURL: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-04/23/17/enhanced/webdr15/enhanced-6210-1429824367-3.jpg?downsize=700:*&output-format=auto&output-quality=auto',
+    name: 'Bob Ross',
+    sex: 'male',
+    age: '6',
+    type: 'dog',
+    breed: 'Box dog',
+    description: 'Standing less than a foot tall, these sturdy terrier-like dogs approach life with great confidence. “This isn’t a breed you train,” a professional dog handler tells us, “He’s like a human. You befriend him.” The dense, harsh coat is described as “neat but shaggy” and comes in several colors; the gait is light and confident. They can be willful and domineering, but mostly Affens are loyal, affectionate, and always entertaining. Affen people say they love being owned by their little monkey dogs.',
+    adoption: true
+  },
+  {
+    imgURL: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-04/23/17/enhanced/webdr15/enhanced-6210-1429824367-3.jpg?downsize=700:*&output-format=auto&output-quality=auto',
+    name: 'Bob Ross',
+    sex: 'male',
+    age: '6',
+    type: 'dog',
+    breed: 'Box dog',
+    description: 'Standing less than a foot tall, these sturdy terrier-like dogs approach life with great confidence. “This isn’t a breed you train,” a professional dog handler tells us, “He’s like a human. You befriend him.” The dense, harsh coat is described as “neat but shaggy” and comes in several colors; the gait is light and confident. They can be willful and domineering, but mostly Affens are loyal, affectionate, and always entertaining. Affen people say they love being owned by their little monkey dogs.',
+    adoption: true
+  },
+  {
+    imgURL: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-04/23/17/enhanced/webdr15/enhanced-6210-1429824367-3.jpg?downsize=700:*&output-format=auto&output-quality=auto',
+    name: 'Bob Ross',
+    sex: 'male',
+    age: '6',
+    type: 'dog',
+    breed: 'Box dog',
+    description: 'Standing less than a foot tall, these sturdy terrier-like dogs approach life with great confidence. “This isn’t a breed you train,” a professional dog handler tells us, “He’s like a human. You befriend him.” The dense, harsh coat is described as “neat but shaggy” and comes in several colors; the gait is light and confident. They can be willful and domineering, but mostly Affens are loyal, affectionate, and always entertaining. Affen people say they love being owned by their little monkey dogs.',
+    adoption: true
+  },
+  {
+    imgURL: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-04/23/17/enhanced/webdr15/enhanced-6210-1429824367-3.jpg?downsize=700:*&output-format=auto&output-quality=auto',
+    name: 'Bob Ross',
+    sex: 'male',
+    age: '6',
+    type: 'dog',
+    breed: 'Box dog',
+    description: 'Standing less than a foot tall, these sturdy terrier-like dogs approach life with great confidence. “This isn’t a breed you train,” a professional dog handler tells us, “He’s like a human. You befriend him.” The dense, harsh coat is described as “neat but shaggy” and comes in several colors; the gait is light and confident. They can be willful and domineering, but mostly Affens are loyal, affectionate, and always entertaining. Affen people say they love being owned by their little monkey dogs.',
+    adoption: true
+  },
+  {
+    imgURL: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-04/23/17/enhanced/webdr15/enhanced-6210-1429824367-3.jpg?downsize=700:*&output-format=auto&output-quality=auto',
+    name: 'Bob Ross',
+    sex: 'male',
+    age: '6',
+    type: 'dog',
+    breed: 'Box dog',
+    description: 'Standing less than a foot tall, these sturdy terrier-like dogs approach life with great confidence. “This isn’t a breed you train,” a professional dog handler tells us, “He’s like a human. You befriend him.” The dense, harsh coat is described as “neat but shaggy” and comes in several colors; the gait is light and confident. They can be willful and domineering, but mostly Affens are loyal, affectionate, and always entertaining. Affen people say they love being owned by their little monkey dogs.',
+    adoption: true
+  },
+  {
+    imgURL: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-04/23/17/enhanced/webdr15/enhanced-6210-1429824367-3.jpg?downsize=700:*&output-format=auto&output-quality=auto',
+    name: 'Bob Ross',
+    sex: 'male',
+    age: '6',
+    type: 'dog',
+    breed: 'Box dog',
+    description: 'Standing less than a foot tall, these sturdy terrier-like dogs approach life with great confidence. “This isn’t a breed you train,” a professional dog handler tells us, “He’s like a human. You befriend him.” The dense, harsh coat is described as “neat but shaggy” and comes in several colors; the gait is light and confident. They can be willful and domineering, but mostly Affens are loyal, affectionate, and always entertaining. Affen people say they love being owned by their little monkey dogs.',
+    adoption: true
+  }
+];
+
+export {array};
