ddbb4416d1ff7516e89449fdefcfc2951bbaadaa
Reldalk <Reldalk@gmail.com>
2018-12-21 12:40:20 -0500

    stuff

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,12 +2,6 @@ import React, { Component } from 'react';
 import './Dashboard.css';
 
 export default class Dashboard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      petArray: [],
-    };
-  }
   render() {
     return (
       <div className = "body">
@@ -25,21 +19,27 @@ export default class Dashboard extends Component {
         <table className="petTable">
           <tbody>
             <tr>
-              {}
-              <td className="petImage">
-                <img className="image" src="https://img.buzzfeed.com/buzzfeed-static/static/2015-04/23/17/enhanced/webdr15/enhanced-6210-1429824367-3.jpg?downsize=700:*&output-format=auto&output-quality=auto"></img>
-              </td>
-              <td className="petDescription">
-                <h2 className="header">Testing</h2>
-                <p>Lorem Ipsum Description Text awodihaowie awdeijaw d wad wad wa dwa d awd w</p>
-                <button className="adoptMe">Adopt Me!</button>
-              </td>
-              <td className="petImage">
-              <img className="image" src="https://img.buzzfeed.com/buzzfeed-static/static/2015-04/23/17/enhanced/webdr15/enhanced-6210-1429824367-3.jpg?downsize=700:*&output-format=auto&output-quality=auto"></img>
-              </td>
-              <td className="petDescription">
-
-              </td>
+              {
+                this.state.array.map(function(i){
+                  return(
+                    <div>
+                      <td className="petImage">
+                        <img className="image" src="https://img.buzzfeed.com/buzzfeed-static/static/2015-04/23/17/enhanced/webdr15/enhanced-6210-1429824367-3.jpg?downsize=700:*&output-format=auto&output-quality=auto"></img>
+                      </td>
+                      <td className="petDescription">
+                        <h2 className="header">Testing</h2>
+                        <p>Lorem Ipsum Description Text awodihaowie awdeijaw d wad wad wa dwa d awd w</p>
+                        <button className="adoptMe">Adopt Me!</button>
+                      </td>
+                      <td className="petImage">
+                        <img className="image" src="https://img.buzzfeed.com/buzzfeed-static/static/2015-04/23/17/enhanced/webdr15/enhanced-6210-1429824367-3.jpg?downsize=700:*&output-format=auto&output-quality=auto"></img>
+                      </td>
+                      <td className="petDescription">
+                      </td>
+                  </div>
+                  )
+                })
+              }
             </tr>
           </tbody>
         </table>
